f023c07998ebf34f4f2ae9e599ebf127694815f0
Mtolentin <mattca701@gmail.com>
2020-06-25 17:56:50 -0700

    250756

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "static_pages#root"
 
-  namespace :api, defaults: { format: :json } do
-    resources :posts, except: [:new, :edit]
+  namespace :api, defaults: {format: :json} do
+    resource :session, :only [:create, :destroy]
+    resources :users, only: [:index, :show, :create, :update]
+      resources :boards, only: [:index, :show, :create, :update, :destroy]
   end
+
+  resources :tins, only: [:index, :show, :create, :update, :destroy]
+  resources :canns, only: [:create, :destroy]
+
+  root to: 'static_pages#root'
+
+
+
+
+
 end
